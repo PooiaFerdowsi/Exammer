75d5b439de4488978e562bec0b2e44e5e1b4098e
PooiaFerdowsi <pooia.ferdowsi.is.developer@gmail.com>
2022-01-30 13:51:48 +0330

    Delete Exammer directory

--- Exammer/errors.py
+++
@@ -1,5 +0,0 @@
-class DB:
-    "A container class for database-related errors"
-    class MultipleObjectsReturnedException(Exception):
-        "Raise it if a method excepted to return one, returned two"
-        pass
